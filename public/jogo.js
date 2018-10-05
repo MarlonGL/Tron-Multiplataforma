db30a55dbb4d178d1ff693c365dfabefd19788f5
MarlonGL <MarlonGL@users.noreply.github.com>
2018-10-04 21:19:33 -0300

    Server funfando e reinicia

--- public/jogo.js
+++ public/jogo.js
@@ -7,6 +7,7 @@ var cauda = [{x:px,y:py}];
 var inimigo = [];
 var comecou = false;
 var perdeu = false;
+var fim = false;
 var sock = io();
 var id = null;
 
@@ -15,14 +16,21 @@ var pontos2 = 0;
 window.onload = function() {
     canvas = document.getElementById('game');
     ctx = canvas.getContext("2d");
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
     
+
     sock.on('id', (idR) => {
-        id = idR;
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, canvas.width, 80);
+        perdeu = false;
+        if(id == null)
+        {
+            id = idR;
+        }
         if(id == 1) {
             px = 10;
-            py = 25;
+            py = 40;
             cauda[0] = { x: px, y: py };
             ctx.fillStyle = "lime";
             ctx.fillRect(cauda[0].x * gs, cauda[0].y * gs, gs - 2, gs - 2);
@@ -31,7 +39,7 @@ window.onload = function() {
         }
         else if(id == 2) {
             px = 70;
-            py = 25;
+            py = 40;
             cauda[0] = { x: px, y: py };
             ctx.fillStyle = "lime";
             ctx.fillRect(cauda[0].x * gs, cauda[0].y * gs, gs - 2, gs - 2);
@@ -45,12 +53,16 @@ window.onload = function() {
         if(id == 1)
         {
             pontos = p1;
+            pontos2 = p2;
+            desenharPontos();
             xv = 1; yv = 0;
             comecou = true;
             sock.emit('xy', cauda[cauda.length - 1]);
         }
         else if (id == 2) {
             pontos2 = p2;
+            pontos = p1;
+            desenharPontos();
             xv = -1; yv = 0;
             comecou = true;
             sock.emit('xy', cauda[cauda.length - 1]);
@@ -65,12 +77,92 @@ window.onload = function() {
             pontos = p1;
             pontos2 = p2;
             console.log('Player 1 tem: ' + pontos + ' Player 2 tem: ' + pontos2);
+            ctx.fillStyle = "black";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            desenharPontos();
+            px = 10;
+            py = 40;
+            cauda = [];
+            inimigo = [];
+            cauda[0] = { x: px, y: py };
+            xv = 1; yv = 0;
+            comecou = true;
+            perdeu = false;
+            ctx.fillStyle = "lime";
+            ctx.fillRect(cauda[0].x * gs, cauda[0].y * gs, gs - 2, gs - 2);
+            sock.emit('xy', cauda[cauda.length - 1]);
         }else if(id == 2) {
             perdeu = true;
             pontos2 = p2;
             pontos = p1;
             console.log('Player 1 tem: ' + pontos + ' Player 2 tem: ' + pontos2);
+            ctx.fillStyle = "black";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            desenharPontos();
+            px = 70;
+            py = 40;
+            cauda = [];
+            inimigo = [];
+            cauda[0] = { x: px, y: py };
+            xv = -1; yv = 0;
+            comecou = true;
+            perdeu = false;
+            ctx.fillStyle = "lime";
+            ctx.fillRect(cauda[0].x * gs, cauda[0].y * gs, gs - 2, gs - 2);
+            sock.emit('xy', cauda[cauda.length - 1]);
         }
+    })
+
+    sock.on('acabou', (p1,p2) => {
+        if(id == 1)
+        {
+            pontos = p1;
+            pontos2 = p2;
+            ctx.fillStyle = "black";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            desenharPontos();
+            comecou = false;
+            cauda = [];
+            inimigo = [];
+            if(pontos == 3)
+            {
+                console.log('vc ganhou mermao');
+                ctx.fillStyle = "lime";
+                desenharTexto('Ganhou', "80px Arial");
+                textozao('Aperte espaço para reiniciar', "40px Arial");
+                fim = true;
+            }else if (pontos2 == 3) {
+                console.log('vc perdeu vai pra casa estudar');
+                ctx.fillStyle = "red";
+                desenharTexto('Perdeu', "80px Arial");
+                textozao('Aperte espaço para reiniciar', "40px Arial");
+                fim = true;
+            }
+            
+        } else if(id == 2) {
+            pontos2 = p2;
+            pontos = p1;
+            ctx.fillStyle = "black";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            desenharPontos();
+            comecou = false;
+            cauda = [];
+            inimigo = [];
+            if(pontos2 == 3)
+            {
+                console.log('vc ganhou mermao');
+                ctx.fillStyle = "lime";
+                desenharTexto('Ganhou', "80px Arial");
+                textozao('Aperte espaço para reiniciar', "40px Arial");
+                fim = true;
+            }else if (pontos == 3) {
+                console.log('vc perdeu vai pra casa estudar');
+                ctx.fillStyle = "red";
+                desenharTexto('Perdeu', "80px Arial");
+                textozao('Aperte espaço para reiniciar', "40px Arial");
+                fim = true;
+            }
+        }   
     })
 
     document.addEventListener("keydown", teclado);
@@ -107,11 +199,48 @@ function game() {
     }
 }
 
+function desenharPontos()
+{
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, canvas.width, 80);
+    ctx.font = "40px Arial";
+    if(id == 1)
+    {
+        ctx.fillStyle = "lime";
+        ctx.fillText(pontos,50,55);
+        ctx.fillStyle = "tomato";
+        ctx.fillText(pontos2,750,55);
+    } else if(id == 2)
+    {
+        ctx.fillStyle = "tomato";
+        ctx.fillText(pontos,50,55);
+        ctx.fillStyle = "lime";
+        ctx.fillText(pontos2,750,55);
+    }
+}
+
+function desenharTexto(texto, fonte)
+{
+    ctx.font = "80px Arial";
+    ctx.font = fonte;
+    ctx.textAlign = "center";
+    ctx.fillText(texto,canvas.width /2,canvas.height /2);
+}
+
+function textozao(text, font) 
+{
+    ctx.font = font;
+    ctx.textAlign = "center";
+    ctx.fillText(text,canvas.width /2,canvas.height /2 + 50);
+
+}
+
 function testarColisao(index) {
     if(cauda[cauda.length - 1].x * gs == cauda[index].x * gs
         && cauda[cauda.length - 1]. y * gs == cauda[index].y * gs) {
             perdeu = true;
             console.log('colidiu consigo');
+            sock.emit('perdi', id);
         }
 }
 
@@ -121,10 +250,14 @@ function testarColisaoInimigo(index) {
             if(index == inimigo.length -1)
             {
                 perdeu = true;
-                console.log('empate')
+                console.log('empate');
+                sock.emit('empate', id);
+            } else {
+                perdeu = true;
+                console.log('colidiu inimigo');
+                sock.emit('perdi', id);
             }
-            perdeu = true;
-            console.log('colidiu inimigo');
+            
         }
 }
 
@@ -141,7 +274,7 @@ function testarColisaoParedes(){
         perdeu = true;
         console.log('parede');
         sock.emit('perdi', id);
-    } else if(cauda[cauda.length - 1]. y * gs == 0){
+    } else if(cauda[cauda.length - 1]. y * gs == 80){
         perdeu = true;
         console.log('parede');
         sock.emit('perdi', id);
@@ -157,16 +290,36 @@ function desenharInimigo(data) {
 function teclado(evt) {
     if (comecou == true) {
         if (evt.keyCode == 37) {
-            xv = -1; yv = 0;
+            if(xv != 1)
+            {
+                xv = -1; yv = 0;
+            }
         }
         if (evt.keyCode == 38) {
-            xv = 0; yv = -1;
+            if(yv != 1)
+            {
+                xv = 0; yv = -1;
+            }
         }
         if (evt.keyCode == 39) {
-            xv = 1; yv = 0;
+            if(xv != -1)
+            {
+                xv = 1; yv = 0;
+            }
         }
         if (evt.keyCode == 40) {
-            xv = 0; yv = 1;
+            if(yv != -1)
+            {
+                xv = 0; yv = 1;
+            }
+        }
+    }
+    if(fim == true){
+        if (evt.keyCode == 32) {
+            sock.emit('restart', id);
+            console.log('quero recomeçar, esperando adversario');
+            fim = false;
+            
         }
     }
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var jogadores = [];
 var idConect = 0;
 var p1 = 0;
 var p2 = 0;
+var resta = 0;
 
 var app = express();
 var server = app.listen(3000);
@@ -31,8 +32,13 @@ function newConnection(sock) {
     sock.on('xy', receber);
     function receber(data) {
         sock.broadcast.emit('xy', data);
-        //console.log(data);
     }
+
+    sock.on('empate', empate);
+    function empate() {
+        io.emit('pontos', p1, p2);
+    }
+
     sock.on('perdi', pontos);
     function pontos(id) {
         if(id == 1) {
@@ -42,6 +48,28 @@ function newConnection(sock) {
             p1++;
             console.log(p1);
         }
-        io.emit('pontos', p1, p2);
+        if(p1 < 3 && p2 < 3)
+        {
+            io.emit('pontos', p1, p2);
+        } else if(p1 == 3 || p2 == 3)
+        {
+            io.emit('acabou', p1, p2);
+        }
+    }
+
+    sock.on('restart', restart);
+    function restart() {
+        resta ++;
+        if(resta == 2)
+        {
+            resta = 0;
+            p1 = 0;
+            p2 = 0;
+            idConect = 1;
+            io.sockets.connected[jogadores[0]].emit('id', idConect);
+            idConect++;
+            io.sockets.connected[jogadores[1]].emit('id', idConect);
+            io.emit('comece', true, p1, p2);
+        }
     }
 }
